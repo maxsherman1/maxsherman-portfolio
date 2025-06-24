ebc1e1b4378749700e896a9ef6ffea85d6a86396
maxsherman1 <maxs.murry@hotmail.com>
2025-06-25 00:04:20 +0100

    Updated layout to be fully responsive for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
           <p className="text-xl mb-8 max-w-2xl mx-auto text-gray-700 dark:text-gray-300">
             Hi, I am Max Sherman, a passionate software developer with expertise in web development, specializing in React and Next.js.
           </p>
-          <div className="flex justify-center space-x-6 mb-8">
-            <Link href="/resume/en_software.pdf" target="_blank" rel="noopener noreferrer" className="bg-[var(--accent)] text-white px-6 py-3 rounded-lg font-medium shadow-md hover:shadow-lg hover:scale-105 hover:bg-[var(--accent-opacity)] transition-all duration-300">
+          <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-6 mb-8">
+            <Link href="/resume/en_software.pdf" target="_blank" rel="noopener noreferrer" className="bg-[var(--accent)] text-white px-6 py-3 rounded-lg font-medium shadow-md hover:shadow-lg hover:scale-105 hover:bg-[var(--accent-opacity)] transition-all duration-300 w-1/3 sm:w-auto self-center">
               Download CV
             </Link>
-            <Link href="https://github.com/maxsherman1" target="_blank" rel="noopener noreferrer" className="bg-[var(--secondary-accent)] text-white px-6 py-3 rounded-lg font-medium shadow-md hover:shadow-lg hover:scale-105 hover:bg-[var(--secondary-accent-opacity)] transition-all duration-300">
+            <Link href="https://github.com/maxsherman1" target="_blank" rel="noopener noreferrer" className="bg-[var(--secondary-accent)] text-white px-6 py-3 rounded-lg font-medium shadow-md hover:shadow-lg hover:scale-105 hover:bg-[var(--secondary-accent-opacity)] transition-all duration-300 w-1/3 sm:w-auto self-center">
               GitHub
             </Link>
-            <Link href="https://www.linkedin.com/in/maxsherman1/" target="_blank" rel="noopener noreferrer" className="bg-[var(--secondary-accent)] text-white px-6 py-3 rounded-lg font-medium shadow-md hover:shadow-lg hover:scale-105 hover:bg-[var(--secondary-accent-opacity)] transition-all duration-300">
+            <Link href="https://www.linkedin.com/in/maxsherman1/" target="_blank" rel="noopener noreferrer" className="bg-[var(--secondary-accent)] text-white px-6 py-3 rounded-lg font-medium shadow-md hover:shadow-lg hover:scale-105 hover:bg-[var(--secondary-accent-opacity)] transition-all duration-300 w-1/3 sm:w-auto self-center">
               LinkedIn
             </Link>
           </div>

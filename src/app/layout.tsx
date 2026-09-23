f38ae1e0086e664f985a53891853ca2012a051a4
maxsherman1 <maxs.murry@hotmail.com>
2026-09-23 22:05:00 +0200

    Added padding for header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} flex flex-col min-h-screen`} suppressHydrationWarning>
         <Navbar />
-        <main className="flex-grow">
+        <main className="flex-grow pt-16">
           {children}
         </main>
         <Footer />
